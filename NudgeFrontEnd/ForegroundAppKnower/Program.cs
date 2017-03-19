0c758518233dd81c558458eab95408727056130c
Sammy Guergachi <sguergachi@gmail.com>
2017-03-19 01:43:10 -0400

    API works

---
+++ NudgeFrontEnd/ForegroundAppKnower/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NudgeToaster;
+
+namespace ForegroundAppKnower
+{
+    class Program
+    {
+        public static Timer MyTimer { get; set; }
+        private static Process _realProcess;
+        static void Main(string[] args)
+        {
+            MyTimer = new Timer(TimerCallback, null, 0, 1000);
+            Console.ReadKey();
+        }
+
+        private static void TimerCallback(object state)
+        {
+            var foregroundProcess = Process.GetProcessById(WinAPIFunctions.GetWindowProcessId(WinAPIFunctions.GetforegroundWindow()));
+            if (foregroundProcess.ProcessName == "ApplicationFrameHost")
+            {
+                foregroundProcess = GetRealProcess(foregroundProcess);
+            }
+            Console.WriteLine(foregroundProcess.ProcessName);
+        }
+
+        private static Process GetRealProcess(Process foregroundProcess)
+        {
+            WinAPIFunctions.EnumChildWindows(foregroundProcess.MainWindowHandle, ChildWindowCallback, IntPtr.Zero);
+            return _realProcess;
+        }
+
+        private static bool ChildWindowCallback(IntPtr hwnd, IntPtr lparam)
+        {
+            var process = Process.GetProcessById(WinAPIFunctions.GetWindowProcessId(hwnd));
+            if (process.ProcessName != "ApplicationFrameHost")
+            {
+                _realProcess = process;
+            }
+            return true;
+        }
+    }
+}
--- NudgeFrontEnd/NudgeToaster/App.xaml.cs
+++ NudgeFrontEnd/NudgeToaster/App.xaml.cs
@@ -79,6 +79,36 @@ namespace NudgeToaster
             }
         }
 
+
+        /// <summary>
+        /// Invoked when the application is launched through a custom URI scheme, such as
+        /// is the case in an OAuth 2.0 authorization flow.
+        /// </summary>
+        /// <param name="args">Details about the URI that activated the app.</param>
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            // When the app was activated by a Protocol (custom URI scheme), forwards
+            // the URI to the MainPage through a Navigate event.
+            if (args.Kind == ActivationKind.Protocol)
+            {
+                // Extracts the authorization response URI from the arguments.
+                ProtocolActivatedEventArgs protocolArgs = (ProtocolActivatedEventArgs)args;
+                Uri uri = protocolArgs.Uri;
+                System.Diagnostics.Debug.WriteLine("Authorization Response: " + uri.AbsoluteUri);
+
+                // Gets the current frame, making one if needed.
+                var frame = Window.Current.Content as Frame;
+                if (frame == null)
+                    frame = new Frame();
+
+                // Opens the URI for "navigation" (handling) on the MainPage.
+                frame.Navigate(typeof(MainPage), uri);
+                Window.Current.Content = frame;
+                Window.Current.Activate();
+            }
+        }
+        
+
         /// <summary>
         /// Invoked when Navigation to a certain page fails
         /// </summary>
