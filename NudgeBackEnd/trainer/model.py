8919a32b399e0b279fddc63b1d18a1674bf0eb2a
aguergachi <bguergachi@gmail.com>
2017-08-03 00:05:35 -0400

    Changed backend code

--- NudgeBackEnd/trainer/model.py
+++ NudgeBackEnd/trainer/model.py
@@ -1,246 +1,50 @@
-# Copyright 2016 Google Inc. All Rights Reserved. Licensed under the Apache
-# License, Version 2.0 (the "License"); you may not use this file except in
-# compliance with the License. You may obtain a copy of the License at
-# http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations under
-# the License.
-
-"""Define a Wide + Deep model for classification on structured data."""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import multiprocessing
-
 import tensorflow as tf
+import numpy as np
+import pandas as pd
 
 
-# Define the format of your input data including unused columns
-CSV_COLUMNS = ['mouse_activity', 'keyboard_activity', 'time_last_request', 'foreground_app']
-CSV_COLUMN_DEFAULTS = [[0], [0], [0], ['']]
-LABEL_COLUMN = 'productivity'
-LABELS = ['0', '1']
+# Data sets
+TF_DATA_FILE = "../data/tf_data.csv"
+TRAINING = "../data/train.csv"
+TEST = "../data/test.csv"
 
-# Define the initial ingestion of each feature used by your model.
-# Additionally, provide metadata about the feature.
-INPUT_COLUMNS = [
-    # Categorical base columns
+#Creating dataset
+dset = pd.read_csv(TF_DATA_FILE,sep=",",usecols=('mouse_activity','keyboard_activity','time_last_request','foreground_app','productive'))
+print ("datasize",dset.size)
+dset.replace('', np.nan, inplace = True)
+dset.dropna(inplace=True)
+dset.to_csv(TRAINING,sep=',',index=False,header=False)
 
-    # Continuous base columns.
-    tf.feature_column.numeric_column('mouse_activity'),
-    tf.feature_column.numeric_column('keyboard_activity'),
-    tf.feature_column.numeric_column('time_last_request'),
+# Load datasets.
+training_set = tf.contrib.learn.datasets.base.load_csv_without_header (filename=TRAINING,
+                                                       target_dtype=np.int, features_dtype=np.float32)
+test_set = tf.contrib.learn.datasets.base.load_csv_without_header(filename=TEST,
+                                                   target_dtype=np.int, features_dtype=np.float32)
 
-    tf.feature_column.categorical_column_with_hash_bucket(
-        'foreground_app', hash_bucket_size=1000, dtype=tf.string),
-]
+#feature columns
+feature_columns = [tf.contrib.layers.real_valued_column("", dimension=4)]
 
-UNUSED_COLUMNS = set(CSV_COLUMNS) - {col.name for col in INPUT_COLUMNS} - \
-    {LABEL_COLUMN}
+# Build 3 layer DNN with 10, 20, 10 units respectively.
+classifier = tf.contrib.learn.DNNClassifier(hidden_units=[10, 10],
+                                            n_classes=2,
+                                            model_dir="diabetes_model",feature_columns=feature_columns)
+print('Training Neural Network....')
+# Fit model.
+classifier.fit(x=training_set.data,
+               y=training_set.target,
+               steps=100000)
+print('Training completed!')
 
-
-def build_estimator(config, embedding_size=8, hidden_units=None):
-  """Build a wide and deep model for predicting income category.
-
-  Wide and deep models use deep neural nets to learn high level abstractions
-  about complex features or interactions between such features.
-  These models then combined the outputs from the DNN with a linear regression
-  performed on simpler features. This provides a balance between power and
-  speed that is effective on many structured data problems.
-
-  You can read more about wide and deep models here:
-  https://research.googleblog.com/2016/06/wide-deep-learning-better-together-with.html
-
-  To define model we can use the prebuilt DNNCombinedLinearClassifier class,
-  and need only define the data transformations particular to our dataset, and
-  then
-  assign these (potentially) transformed features to either the DNN, or linear
-  regression portion of the model.
-
-  Args:
-    config: tf.contrib.learn.RunConfig defining the runtime environment for the
-      estimator (including model_dir).
-    embedding_size: int, the number of dimensions used to represent categorical
-      features when providing them as inputs to the DNN.
-    hidden_units: [int], the layer sizes of the DNN (input layer first)
-    learning_rate: float, the learning rate for the optimizer.
-  Returns:
-    A DNNCombinedLinearClassifier
-  """
-  (mouse_activity, keyboard_activity, time_last_request, foreground_app) = INPUT_COLUMNS
-  """Build an estimator."""
-
-  # Reused Transformations.
-  # Continuous columns can be converted to categorical via bucketization
-  #age_buckets = tf.feature_column.bucketized_column(
-  #    age, boundaries=[18, 25, 30, 35, 40, 45, 50, 55, 60, 65])
-
-  # Feature columns.
-  feature_columns = [
-      mouse_activity,
-      keyboard_activity,
-      time_last_request,
-      foreground_app
-  ]
-
-  return tf.contrib.learn.DNNLinearCombinedClassifier(
-      config=config,
-      feature_columns=feature_columns,
-      dnn_hidden_units=hidden_units or [100, 70, 50, 25],
-      fix_global_step_increment_bug=True
-  )
+# Evaluate accuracy.
+accuracy_score = classifier.evaluate(x=test_set.data,
+                                     y=test_set.target)["accuracy"]
+print('Accuracy: {0:f}'.format(accuracy_score))
 
 
-def parse_label_column(label_string_tensor):
-  """Parses a string tensor into the label tensor
-  Args:
-    label_string_tensor: Tensor of dtype string. Result of parsing the
-    CSV column specified by LABEL_COLUMN
-  Returns:
-    A Tensor of the same shape as label_string_tensor, should return
-    an int64 Tensor representing the label index for classification tasks,
-    and a float32 Tensor representing the value for a regression task.
-  """
-  # Build a Hash Table inside the graph
-  table = tf.contrib.lookup.index_table_from_tensor(tf.constant(LABELS))
+# Classify two new patient samples.
+new_samples = np.array(
+    [[0,2,31638,'devenv']], dtype=float)
+y = classifier.predict(new_samples,as_iterable=False)
 
-  # Use the hash table to convert string labels to ints and one-hot encode
-  return table.lookup(label_string_tensor)
-
-
-# ************************************************************************
-# YOU NEED NOT MODIFY ANYTHING BELOW HERE TO ADAPT THIS MODEL TO YOUR DATA
-# ************************************************************************
-
-
-def csv_serving_input_fn():
-  """Build the serving inputs."""
-  csv_row = tf.placeholder(
-      shape=[None],
-      dtype=tf.string
-  )
-  features = parse_csv(csv_row)
-  features.pop(LABEL_COLUMN)
-  return tf.contrib.learn.InputFnOps(features, None, {'csv_row': csv_row})
-
-
-def example_serving_input_fn():
-  """Build the serving inputs."""
-  example_bytestring = tf.placeholder(
-      shape=[None],
-      dtype=tf.string,
-  )
-  feature_scalars = tf.parse_example(
-      example_bytestring,
-      tf.feature_column.make_parse_example_spec(INPUT_COLUMNS)
-  )
-  features = {
-      key: tf.expand_dims(tensor, -1)
-      for key, tensor in feature_scalars.iteritems()
-  }
-  return tf.contrib.learn.InputFnOps(
-      features,
-      None,  # labels
-      {'example_proto': example_bytestring}
-  )
-
-
-def json_serving_input_fn():
-  """Build the serving inputs."""
-  inputs = {}
-  for feat in INPUT_COLUMNS:
-    inputs[feat.name] = tf.placeholder(shape=[None], dtype=feat.dtype)
-
-  features = {
-      key: tf.expand_dims(tensor, -1)
-      for key, tensor in inputs.iteritems()
-  }
-  return tf.contrib.learn.InputFnOps(features, None, inputs)
-
-
-SERVING_FUNCTIONS = {
-    'JSON': json_serving_input_fn,
-    'EXAMPLE': example_serving_input_fn,
-    'CSV': csv_serving_input_fn
-}
-
-
-def parse_csv(rows_string_tensor):
-  """Takes the string input tensor and returns a dict of rank-2 tensors."""
-
-  # Takes a rank-1 tensor and converts it into rank-2 tensor
-  # Example if the data is ['csv,line,1', 'csv,line,2', ..] to
-  # [['csv,line,1'], ['csv,line,2']] which after parsing will result in a
-  # tuple of tensors: [['csv'], ['csv']], [['line'], ['line']], [[1], [2]]
-  row_columns = tf.expand_dims(rows_string_tensor, -1)
-  columns = tf.decode_csv(row_columns, record_defaults=CSV_COLUMN_DEFAULTS)
-  features = dict(zip(CSV_COLUMNS, columns))
-
-  # Remove unused columns
-  for col in UNUSED_COLUMNS:
-    features.pop(col)
-  return features
-
-
-def generate_input_fn(filenames,
-                      num_epochs=None,
-                      shuffle=True,
-                      skip_header_lines=0,
-                      batch_size=200):
-  """Generates an input function for training or evaluation.
-  This uses the input pipeline based approach using file name queue
-  to read data so that entire data is not loaded in memory.
-
-  Args:
-      filenames: [str] list of CSV files to read data from.
-      num_epochs: int how many times through to read the data.
-        If None will loop through data indefinitely
-      shuffle: bool, whether or not to randomize the order of data.
-        Controls randomization of both file order and line order within
-        files.
-      skip_header_lines: int set to non-zero in order to skip header lines
-        in CSV files.
-      batch_size: int First dimension size of the Tensors returned by
-        input_fn
-  Returns:
-      A function () -> (features, indices) where features is a dictionary of
-        Tensors, and indices is a single Tensor of label indices.
-  """
-  filename_queue = tf.train.string_input_producer(
-      filenames, num_epochs=num_epochs, shuffle=shuffle)
-  reader = tf.TextLineReader(skip_header_lines=skip_header_lines)
-
-  _, rows = reader.read_up_to(filename_queue, num_records=batch_size)
-
-  # Parse the CSV File
-  features = parse_csv(rows)
-
-  # This operation builds up a buffer of parsed tensors, so that parsing
-  # input data doesn't block training
-  # If requested it will also shuffle
-  if shuffle:
-    features = tf.train.shuffle_batch(
-        features,
-        batch_size,
-        min_after_dequeue=2 * batch_size + 1,
-        capacity=batch_size * 10,
-        num_threads=multiprocessing.cpu_count(),
-        enqueue_many=True,
-        allow_smaller_final_batch=True
-    )
-  else:
-    features = tf.train.batch(
-        features,
-        batch_size,
-        capacity=batch_size * 10,
-        num_threads=multiprocessing.cpu_count(),
-        enqueue_many=True,
-        allow_smaller_final_batch=True
-    )
-
-  return features, parse_label_column(features.pop(LABEL_COLUMN))
+print('Inputs: {}'.format(new_samples))
+print ('Predictions: {}'.format(y))
