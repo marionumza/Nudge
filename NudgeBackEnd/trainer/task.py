711ac1f9cc3f22a1512d5bbc6b82ec180a04ef2d
aguergachi <bguergachi@gmail.com>
2017-08-18 23:14:53 -0400

    backend update

--- NudgeBackEnd/trainer/task.py
+++ NudgeBackEnd/trainer/task.py
@@ -62,115 +62,12 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   # Input Arguments
   parser.add_argument(
-      '--train-files',
-      help='GCS or local paths to training data',
+      '-train_file',
+      help='Path to data on server',
       nargs='+',
-      required=True
-  )
-  parser.add_argument(
-      '--num-epochs',
-      help="""\
-      Maximum number of training data epochs on which to train.
-      If both --max-steps and --num-epochs are specified,
-      the training job will run for --max-steps or --num-epochs,
-      whichever occurs first. If unspecified will run for --max-steps.\
-      """,
-      type=int,
-  )
-  parser.add_argument(
-      '--train-batch-size',
-      help='Batch size for training steps',
-      type=int,
-      default=40
-  )
-  parser.add_argument(
-      '--eval-batch-size',
-      help='Batch size for evaluation steps',
-      type=int,
-      default=40
-  )
-  parser.add_argument(
-      '--eval-files',
-      help='GCS or local paths to evaluation data',
-      nargs='+',
-      required=True
-  )
-  # Training arguments
-  parser.add_argument(
-      '--embedding-size',
-      help='Number of embedding dimensions for categorical columns',
-      default=8,
-      type=int
-  )
-  parser.add_argument(
-      '--first-layer-size',
-      help='Number of nodes in the first layer of the DNN',
-      default=100,
-      type=int
-  )
-  parser.add_argument(
-      '--num-layers',
-      help='Number of layers in the DNN',
-      default=4,
-      type=int
-  )
-  parser.add_argument(
-      '--scale-factor',
-      help='How quickly should the size of the layers in the DNN decay',
-      default=0.7,
-      type=float
-  )
-  parser.add_argument(
-      '--job-dir',
-      help='GCS location to write checkpoints and export models',
       required=True
   )
 
-  # Argument to turn on all logging
-  parser.add_argument(
-      '--verbosity',
-      choices=[
-          'DEBUG',
-          'ERROR',
-          'FATAL',
-          'INFO',
-          'WARN'
-      ],
-      default='INFO',
-  )
-  # Experiment arguments
-  parser.add_argument(
-      '--eval-delay-secs',
-      help='How long to wait before running first evaluation',
-      default=10,
-      type=int
-  )
-  parser.add_argument(
-      '--min-eval-frequency',
-      help='Minimum number of training steps between evaluations',
-      default=None,  # Use TensorFlow's default (currently, 1000 on GCS)
-      type=int
-  )
-  parser.add_argument(
-      '--train-steps',
-      help="""\
-      Steps to run the training job for. If --num-epochs is not specified,
-      this must be. Otherwise the training job will run indefinitely.\
-      """,
-      type=int
-  )
-  parser.add_argument(
-      '--eval-steps',
-      help='Number of steps to run evalution for at each checkpoint',
-      default=100,
-      type=int
-  )
-  parser.add_argument(
-      '--export-format',
-      help='The input format of the exported SavedModel binary',
-      choices=['JSON', 'CSV', 'EXAMPLE'],
-      default='JSON'
-  )
 
   args = parser.parse_args()
 
@@ -179,7 +76,7 @@ if __name__ == '__main__':
   # Set C++ Graph Execution level verbosity
   os.environ['TF_CPP_MIN_LOG_LEVEL'] = str(
       tf.logging.__dict__[args.verbosity] / 10)
-
+ 
   # Run the training job
   # learn_runner pulls configuration information from environment
   # variables using tf.learn.RunConfig and uses this configuration
